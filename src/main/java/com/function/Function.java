f398bd2418caf385fe81fc9229f23f74e47c0141
David L Whitehurst <me@dlwhitehurst.com>
2023-03-18 14:39:57 -0400

    all api resources are implemented and tested

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -23,6 +23,7 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
@@ -37,12 +38,21 @@ public class Function {
      * The functions listen at endpoint "/api/v1/<function>". 
      */
 
-     // curl -i {your host}/api/states
-    @FunctionName("getAndPostStates")
-    public HttpResponseMessage getAndPostStates(
+    /**
+     * This function provides the implementation of:
+     *  - GET /api/v1/states
+     *  - POST /api/v1/states
+     *  - DELETE /api/v1/states
+     * 
+     * @param request
+     * @param context
+     * @return
+     */
+    @FunctionName("handleStates")
+    public HttpResponseMessage handleStates(
             @HttpTrigger(
                 name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
+                methods = {HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE},
                 route = "states",
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<State>> request,
@@ -50,7 +60,7 @@ public class Function {
 
                 context.getLogger().info("Request body is: " + request.getBody().orElse(null));
 
-            if (request.getHttpMethod().equals(HttpMethod.POST)) {
+                if (request.getHttpMethod().equals(HttpMethod.POST)) {
                 // Check request body
                     if (!request.getBody().isPresent()) {
                         return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
@@ -65,12 +75,67 @@ public class Function {
                             .body(body)
                             .build();
                             // need to discuss status code and return e.g. 201 - Created
-                        }
+                    }
+                } else if (request.getHttpMethod().equals(HttpMethod.DELETE)) {
+                    // DELETE ALL (*** WARNING ***)
+                    return request.createResponseBuilder(HttpStatus.NO_CONTENT).build(); // if body sent, will fail
                 } else {
-                    return request.createResponseBuilder(HttpStatus.OK).body("STATUS=200, All states returned.").build();
+                    return request.createResponseBuilder(HttpStatus.OK).body("STATUS: 200 (Success), All states returned.").build();
                 }
             }
+    /**
+     * This function provides the implementation of:
+     *  - GET /api/v1/states/{stateId}
+     *  - DELETE /api/v1/states/{stateId}
+     *  - PUT /api/v1/states/{stateId}
+     * 
+     * @param request
+     * @param context
+     * @return
+     */
+    @FunctionName("getStateById")
+    public HttpResponseMessage getStateById(
+        @HttpTrigger(
+            name = "req",
+            methods = {HttpMethod.GET, HttpMethod.DELETE, HttpMethod.PUT},
+            route = "states/{stateId}/{anything=EMPTY}",
+            authLevel = AuthorizationLevel.ANONYMOUS)
+        HttpRequestMessage<Optional<String>> request,
+            @BindingName("stateId") String id,
+            @BindingName("anything") String anything,
+            final ExecutionContext context) {
 
+                context.getLogger().info("URL parameters are: " + id);
+
+                if (request.getHttpMethod().equals(HttpMethod.GET)) {
+                    // GET
+                    final String name = "New York";
+                    final String code = "NY";
+    
+                    final String jsonDocument = "{\"id\":\"" + id + "\", " + 
+                    "\"name\": \"" + name + "\", " +
+                    "\"code\": \"" + code + "\"}";
+    
+                    return request.createResponseBuilder(HttpStatus.OK)
+                        .header("Content-Type", "application/json")
+                        .body(jsonDocument)
+                        .build();
+                    
+                } else if (request.getHttpMethod().equals(HttpMethod.DELETE)) {
+                    // DELETE
+                    return request.createResponseBuilder(HttpStatus.NO_CONTENT).build(); // if body sent, will fail
+
+                } else if (request.getHttpMethod().equals(HttpMethod.PUT)) {
+                    // PUT
+                    return request.createResponseBuilder(HttpStatus.OK).body("STATUS: 200 (Success), State updated/replaced.").build();
+
+                } else {
+                    // ??? something is wrong
+                    return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("STATUS: 400 (Bad Request)").build();
+                }
+
+    }    
+        
     /**
      * This function is currently used for a single testing example. More effort is needed to provide coverage and
      * automated testing. Address this when time is available.
